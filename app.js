f79a5afa75066cd79be8814626eb4dd9f04b3e21
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-07-18 16:03:51 +1000

    can delete, edit, view, and other minro tweaks

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 if(process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express"),
 app = express(),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+methodOverride = require("method-override");
 
 const Note = require("./models/note");
 const seed = require("./seed");
@@ -11,12 +12,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true});
 
-// temporary seeding of db
-seed();
+// seeds db in debug
+// seed();
 
 app.get("/", async (req, res) => {
     var notes = await Note.find({})
@@ -29,7 +31,8 @@ app.get("/addnote", (req, res) => {
 
 app.get("/viewnote/:noteid", async (req, res) => {
     var note = await Note.findById(req.params.noteid);
-    res.render("viewnotes", {note: note});
+    var realNoteContent = await Note.findById(req.params.noteid);
+    res.render("viewnotes", {note: note, realNoteContent: realNoteContent});
 });
 
 app.post("/addnote", async (req, res) => {
@@ -40,6 +43,15 @@ app.post("/addnote", async (req, res) => {
 app.post("/updatenote/:id", async (req, res) => {
     await Note.findByIdAndUpdate(req.params.id, req.body.updatenote);
     res.redirect("/");
+});
+
+app.delete("/del/:id", async (req, res) => {
+    await Note.findByIdAndRemove(req.params.id);
+    res.redirect("/");
+});
+
+app.get("/*", (req, res) => {
+    res.send("You're not in the right place. <a href='/'>Come back!</a>")
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 // Sets the margin left of the add new notes button.
 // margin-left: auto; doesnt work so this is what i did :/
 setInterval(() => {
-    var mainContainerBtnWidth = document.querySelector(".main-container a").getBoundingClientRect().width;
+    var mainContainerBtnWidth = document.querySelector(".main-container .btn.add-new-note").getBoundingClientRect().width;
     var mainContainerH1Width = document.querySelector(".main-container h1").getBoundingClientRect().width;
     var mainContainerWidth = document.querySelector(".main-container").getBoundingClientRect().width;
     // 48 is the 1.5rem on both sides
     var mainContainerBtnMarginLeft = mainContainerWidth - mainContainerH1Width - mainContainerBtnWidth - 48;
 
-    document.querySelector(".main-container a").style.marginLeft = `${mainContainerBtnMarginLeft}px`;
+    document.querySelector(".main-container .add-new-note").style.marginLeft = `${mainContainerBtnMarginLeft}px`;
 }, 200);
